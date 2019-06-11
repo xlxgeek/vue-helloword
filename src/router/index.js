a9f919b55fb17413a77762652acf6a2d22cea5c5
xlx <1119397060@qq.com>
2019-06-11 21:44:37 +0800

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import BlogLogin from '@/components/manage/BlogLogin.vue'
+import BlogIndex from '@/components/home/BlogIndex.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/index',
+      name: 'BlogIndex',
+      component: BlogIndex
+    },
+    {
+      path: '/manage',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'BlogLogin',
+      component: BlogLogin
+    }
+  ]
+})
